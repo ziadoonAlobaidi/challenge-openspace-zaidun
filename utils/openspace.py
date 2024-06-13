b55a0463b39d360660bf1cc2081f04672e6d35f3
ziadoonAlobaidi <ziadoon.obaidi@gmail.com>
2024-06-13 15:58:03 +0200

    new things

--- utils/openspace.py
+++ utils/openspace.py
@@ -1,10 +1,41 @@
 from table import Table
+from table import Seat
+import numpy as np
 
 class Openspace :
 
     def __init__(self, number_of_tables, seats_per_table):
         self.tables = []
-        self.number_of_tables
+        
+        self.number_of_tables = number_of_tables
+
+
         for x in range(number_of_tables):
             self.tables.append(Table(seats_per_table))
+
+    def organize(names) :
+        """
+        Randomly assign people to Seat objects in the different Table objects.
+        """
+        shuffleNames = np.random.shuffle(names)
+        for name in shuffleNames :
+            Table.assign_seat(name)
+            
+    
+    def display(): 
+        """
+        display the different tables and their occupants in a nice and readable way store(filename) store the repartition in an excel file
+        """
+        for table in self.tables :
+            for seat in table.seats :
+                print(seat.occupent)
+                occupant = seat.occupent
+               self.store(occupant)
         
+    def store(newRecord):
+        """
+        store(filename) store the repartition in an excel file
+        """
+        with open('new.xlsx','w') as f :
+            f.write(newRecord)
+
