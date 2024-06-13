5248f3abe8053abf593895673b4d7e72a55a0927
AlisherJard <alisherjard@gmail.com>
2024-06-13 14:14:02 +0200

    Updates: Table.py (created class Seat)

--- utils/table.py
+++ utils/table.py
@@ -9,6 +9,12 @@ class Seat:
 
     # set_occupant method allows to assign a seat if it's free, otherwise returns message.
     def set_occupant(self, name):
+        """
+        Function that allows to assign a seat if it's free, otherwise returns message.
+
+        :param name: Name of a person that you want to assign a seat.
+        :return: Returns to whom the seat was assigned or if the seat is free.
+        """
         if self.free:
             self.occupant = name
             self.free = False
@@ -18,7 +24,12 @@ class Seat:
 
     # remove_occupant method removes someone from a seat and returns name of the person.
     def remove_occupant(self):
+        """
+        Function that allows to remove someone from a seat and returns name of the person.
 
+        :param self:
+        :return: Returns the name of the person that you want to remove or if seat is free.
+        """
         if self.free is False:
             previous_occupant = self.occupant
             self.occupant = ""
@@ -27,6 +38,16 @@ class Seat:
         if self.free:
             return "Seat is already free"
 
+    # __str__ shows status of an object that belongs to the class (Seat).
+    def __str__(self):
+        """
+        Function that shows status of an object that belongs to the class (Seat).
+
+        :param self:
+        :return: Returns the status of a seat.
+        """
+        status = "free" if self.free else "occupied"
+        return f"Seat is {status}. Occupant: {self.occupant}"
 
 # Creating objects (with True and False) (to check both conditons)(for set_occupant):
 a = Seat(True, '')
@@ -46,3 +67,8 @@ print('\n')
 print(b.remove_occupant())
 print(b2.remove_occupant())
 
+print('\n')
+
+# testing of __str__ method.
+print(a)
+print(a2)
