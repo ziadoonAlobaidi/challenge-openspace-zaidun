cb932e8c05b925b67ff0e8f7107b47b4aaeff53d
ziadoonAlobaidi <ziadoon.obaidi@gmail.com>
2024-06-13 14:21:16 +0200

    add some code

--- utils/openspace.py
+++ utils/openspace.py
@@ -1,1 +1,10 @@
+from table import Table
 
+class Openspace :
+
+    def __init__(self, number_of_tables, seats_per_table):
+        self.tables = []
+        self.number_of_tables
+        for x in range(number_of_tables):
+            self.tables.append(Table(seats_per_table))
+        
