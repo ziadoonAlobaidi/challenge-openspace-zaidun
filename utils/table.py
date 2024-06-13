635016c4cf49ff8d01ffe87fd6c3c6f61909fb24
mehme <mehmetbatar120@gmail.com>
2024-06-13 13:55:59 +0200

    class table from mehmet

--- utils/table.py
+++ utils/table.py
@@ -1,1 +1,18 @@
+# I am gonna create table class in table.py
 
+class Table:
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.seats = [Seat() for i in range(capacity)]
+
+
+    def has_free_spot(self):
+        for seat in self.seats:
+            if seat.free:
+                return True
+        return False         
+    
+    def assign_seat(name):
+        for seat in self.seats:
+            if seat.free:
+                seat.set
