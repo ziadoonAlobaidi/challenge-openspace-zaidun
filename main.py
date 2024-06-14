9a08e092da1f15e19cbf50ef7f7dd63b72f79dbd
AlisherJard <alisherjard@gmail.com>
2024-06-14 11:47:33 +0200

    Updated: main.py

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-from utils.openspace import Openspace
+# from table import Table, Seat
+# from openspace import Openspace
+import pandas as pd
+a = 6
+b = 4
+
+df_names = pd.read_excel('./utils/Example Excel Template.xlsx')
+
+names = df_names.iloc[1:25, 0]
+
+a2 = []
+
+for x in names:
+    a2.append(x)
+print(a2)
+
+
+
